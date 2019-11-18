7f05aa8269dfd5f48e82b51bba63b1989d4cd2ba
mby4github <mby@tut.by>
2019-11-18 16:51:03 +0300

    Edit databse

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,13 @@
 import FluentPostgreSQL
 import Vapor
 
+public struct PostgresDefaults {
+    public static let hostname = "localhost"
+    public static let username = "siarheisuliukou"
+    public static let database = "foodtracker"
+    public static let port = 5432
+}
+
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
@@ -21,11 +28,20 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     //let config = PostgreSQLDatabaseConfig(hostname: "ec2-54-75-245-196.eu-west-1.compute.amazonaws.com", port: 5432, username: "edamsbhmjmcwrq", database: "dbcc2mk522jklo", password: "9d70e78324a22dbc32a4d370110c4d6135124d25a277373068b7495ba5ca7fc9", transport: .cleartext)
     let postgreSQLConfig : PostgreSQLDatabaseConfig
     
-    if let url = Environment.get("DATABASE_URL") {
-        postgreSQLConfig = PostgreSQLDatabaseConfig(url: url)!
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? PostgresDefaults.hostname
+    let username = Environment.get("DATABASE_USERNAME") ?? PostgresDefaults.username
+    let database = Environment.get("DATABASE_DATABASE") ?? PostgresDefaults.database
+    let password = Environment.get("DATABASE_PASSWORD")
+    
+    let port : Int
+    
+    if let portString = Environment.get("DATABASE_PORT") {
+        port = Int(portString) ?? PostgresDefaults.port
     } else {
-        postgreSQLConfig = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "siarheisuliukou", database: "foodtracker", password: nil, transport: .cleartext)
+        port = PostgresDefaults.port
     }
+    
+    postgreSQLConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: port, username: username, database: database, password: password, transport: .cleartext)
     
     let postgres = PostgreSQLDatabase(config: postgreSQLConfig)
     
