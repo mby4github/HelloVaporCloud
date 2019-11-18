cf337a058054a362ea2a28668db7ee8620f8f985
mby4github <mby@tut.by>
2019-11-18 16:13:30 +0300

    Edit databse

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -18,8 +18,16 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
     
     //let config = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "siarheisuliukou", database: "foodtracker", password: nil, transport: .cleartext)
-    let config = PostgreSQLDatabaseConfig(hostname: "ec2-54-75-245-196.eu-west-1.compute.amazonaws.com", port: 5432, username: "edamsbhmjmcwrq", database: "dbcc2mk522jklo", password: "9d70e78324a22dbc32a4d370110c4d6135124d25a277373068b7495ba5ca7fc9", transport: .cleartext)
-    let postgres = PostgreSQLDatabase(config: config)
+    //let config = PostgreSQLDatabaseConfig(hostname: "ec2-54-75-245-196.eu-west-1.compute.amazonaws.com", port: 5432, username: "edamsbhmjmcwrq", database: "dbcc2mk522jklo", password: "9d70e78324a22dbc32a4d370110c4d6135124d25a277373068b7495ba5ca7fc9", transport: .cleartext)
+    let postgreSQLConfig : PostgreSQLDatabaseConfig
+    
+    if let url = Environment.get("DATABASE_URL") {
+        postgreSQLConfig = PostgreSQLDatabaseConfig(url: url)!
+    } else {
+        postgreSQLConfig = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "siarheisuliukou", database: "foodtracker", password: nil, transport: .cleartext)
+    }
+    
+    let postgres = PostgreSQLDatabase(config: postgreSQLConfig)
     
     var databases = DatabasesConfig()
     databases.add(database: postgres, as: .psql)
